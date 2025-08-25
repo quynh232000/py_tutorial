60e9c266d525832d6592db41e2d0bcf78688d56e
quynh <quynh232000@gmail.com>
2025-08-25 22:22:40 +0700

    create template

--- site1/site1/urls.py
+++ site1/site1/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from home import views as home
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home.get_home),
 ]
