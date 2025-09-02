3545c6c241edd6ce4522d063b3ea3c571efecd1d
quynh <quynh232000@gmail.com>
2025-09-02 23:24:41 +0700

    fe: layout template

--- webbanhang/app/urls.py
+++ webbanhang/app/urls.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home),
+    path('', views.home,name='home'),
+    path('shop', views.shop,name='shop'),
+    path('cart', views.cart,name='cart'),
+    path('detail', views.detail,name='detail'),
+    path('testemonial', views.testemonial,name='testemonial'),
+    path('checkout', views.checkout,name='checkout'),
+    path('contact', views.contact,name='contact'),
 ]
--- webbanhang/app/views.py
+++ webbanhang/app/views.py
@@ -3,3 +3,15 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request,'app/home.html')
+def shop(request):
+    return render(request,'app/shop.html')
+def cart(request):
+    return render(request,'app/cart.html')
+def checkout(request):
+    return render(request,'app/checkout.html')
+def testemonial(request):
+    return render(request,'app/testemonial.html')
+def detail(request):
+    return render(request,'app/detail.html')
+def contact(request):
+    return render(request,'app/contact.html')
